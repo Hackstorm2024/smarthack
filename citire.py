e6b63f3b50a95a9fa22c1888b977a8e1b46a85d7
Adrian <109019350+YcePitic@users.noreply.github.com>
2024-11-02 14:25:40 +0200

    Add files via upload

---
+++ citire.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def citire_date():
+
+    raf_df = pd.read_csv('refineries.csv', delimiter=';')
+    conn_df = pd.read_csv('connections.csv', delimiter=';')
+    customers_df = pd.read_csv('customers.csv', delimiter=';')
+    demands_df = pd.read_csv('demands.csv', delimiter=';')
+    tanks_df = pd.read_csv('tanks.csv', delimiter=';')
+    teams_df = pd.read_csv('teams.csv', delimiter=';')
+    
+    return raf_df, conn_df, customers_df, demands_df, tanks_df, teams_df
+
+
+raf_df, conn_df, customers_df, demands_df, tanks_df, teams_df = citire_date()
+
+
+print(teams_df.head(3))
